40530529f37ac0f12c3f2921bc9bd5865d8da8eb
Elli Shim <ellishim@gmail.com>
2018-02-02 09:46:03 -0500

    fixing styling of navbar

--- homedog-frontend/src/App.js
+++ homedog-frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Link, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Login from './components/Login';
-import Homepage from './components/Homepage'
+// import Homepage from './components/Homepage'
 import NavBar from './components/NavBar'
 import * as actions from './actions';
 
@@ -11,11 +11,10 @@ class App extends Component {
     console.log(this.props.loggedIn);
     return (
       <div className="App">
-        <NavBar />
-        
+        <div><NavBar /></div>
+
         <Switch>
           <Route path="/login" component={Login} />
-          {/*<Route path="/profile" component={Profile} />*/}
         </Switch>
       </div>
     );
--- homedog-frontend/src/components/NavBar.js
+++ homedog-frontend/src/components/NavBar.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
-import { Link, Switch, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Login from './Login';
-import Homepage from './Homepage'
+// import Homepage from './Homepage'
 import * as actions from '../actions';
 
 class NavBar extends Component {
   render() {
     return (
-      <ul>
-        <dl>
+      <div className="ui fixed top menu">
+        <h2 className="header item">Homedog</h2>
+        <div>
+          <NavLink to="/">Home</NavLink>
           {this.props.loggedIn ? (
-            <a
+            <div ><a
               onClick={e => {
                 e.preventDefault();
                 this.props.logoutUser();
               }}
+              activeStyle={{
+        'border-bottom': 'solid'
+      }}
             >
               Sign Out
-            </a>
+            </a></div>
           ) : (
-            <Link to="/login">Go to Login</Link>
+            <div ><NavLink to="/login" activeStyle={{
+        'border-bottom': 'solid'
+      }}>Go to Login</NavLink></div>
           )}
-        </dl>
-      </ul>
+        </div>
+      </div>
     )
   }
 }
